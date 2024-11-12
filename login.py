caec47fd3ee165f4d0fc0cb540760ecbc6d91b6d
Mahtiw <mahtipy@gmail.com>
2024-11-12 18:11:11 +0330

    Update login.py

--- login.py
+++ login.py
@@ -4,48 +4,38 @@ import pickle
 import os
 import re
 
-# توکن ربات
 TOKEN = 'YOUR_BOT_TOKEN_HERE'
 
-# اطلاعات پنل‌ها
 panels = [
     {"url": "http://s1.example.com:1111/", "username": "user1", "password": "admin1"},
     {"url": "http://s1.example.com:2222/", "username": "user2", "password": "admin2"},
     {"url": "http://s1.example.com:3333/", "username": "user3", "password": "admin3"},
 ]
 
-# آدرس‌ها
 url_login = "login"
 url_lists = "xui/inbound/list"
 url_sanaei = "panel/inbound/list"
 
-# مسیر فایل پیکلی برای ذخیره کوکی‌ها
 pickle_file = "cookies.pkl"
 json_file = "data.json"
 
-# ذخیره کوکی‌ها در فایل پیکلی
 def write_pickle(pickle_data):
     with open(pickle_file, 'wb') as f:
         pickle.dump(pickle_data, f)
 
-# بررسی وجود فایل
 def file_exists(file_path):
     return os.path.exists(file_path)
 
-# بررسی خالی بودن فایل
 def is_file_empty(file_path):
     return file_exists(file_path) and os.stat(file_path).st_size == 0
 
-# دریافت اطلاعات از پنل‌ها
 def get_data_from_panels():
     panels_data = {}
     cookies = {}
 
-    # اگر فایل پیکلی وجود ندارد، آن را ایجاد کنیم
     if not file_exists(pickle_file):
         open(pickle_file, 'w').close()
 
-    # اگر فایل خالی نیست، کوکی‌ها را بخوانیم
     elif not is_file_empty(pickle_file):
         try:
             with open(pickle_file, 'rb') as f:
@@ -70,15 +60,12 @@ def get_data_from_panels():
 
         session = requests.Session()
 
-        # اگر کوکی موجود بود، آن را به درخواست اضافه کنیم
         if panel_name in cookies:
             session.cookies.update(cookies[panel_name])
 
         try:
-            # ارسال درخواست لاگین
             response = session.post(panel['url'] + url_login, data=payload)
 
-            # بررسی نتیجه لاگین
             if response.status_code == 404:
                 print(f"{panel_name} : Are you sure your panel doesn't have a path address?")
                 continue
@@ -86,11 +73,9 @@ def get_data_from_panels():
                 print(f"{panel_name} : Wrong Username or Password")
                 continue
 
-            # ذخیره کوکی‌ها برای استفاده‌های بعدی
             cookies[panel_name] = response.cookies.get_dict()
             write_pickle(cookies)
 
-            # دریافت داده‌ها پس از لاگین موفقیت‌آمیز
             panel_data = session.post(panel['url'] + url_lists)
             if panel_data.status_code != 404:
                 panels_data[panel_name] = panel_data.json()['obj']
@@ -103,7 +88,6 @@ def get_data_from_panels():
 
     return panels_data
 
-# ذخیره داده‌ها در فایل JSON
 def save_accounts_to_json():
     panels_data = get_data_from_panels()
 
